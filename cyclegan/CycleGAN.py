fd5e2c7f4be91e813248194b5586c1e87028e939
pujols811121 <pujols811121@gmail.com>
2019-12-23 18:56:37 -0600

    12/23/2019

---
+++ cyclegan/CycleGAN.py
@@ -0,0 +1,168 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import random
+import itertools
+from Generator import Generator
+from Discriminator import Discriminator
+
+Tensor = torch.cuda.FloatTensor
+#if opt.cuda else torch.Tensor
+#Tensor = torch.Tensor
+
+class LambdaLR():
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert ((n_epochs - decay_start_epoch) > 0),"Decay must start before the training sessions ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity=50):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+        self.size = 0
+
+    def add_and_sample(self, img, batch_size):
+        if random.random() > 0.5 or self.size < 1:
+            ret = img
+        else:
+            ret = random.sample(self.buffer, batch_size)
+            ret = torch.cat(ret, 0)
+
+        if self.size < self.capacity:
+            self.buffer.append(img)
+        else:
+            self.buffer[self.position] = img
+        self.position = (self.position + 1) % self.capacity
+        self.size += 1
+
+        return ret
+
+class CycleGAN(nn.Module):
+    def __init__(self, opt, is_train=True):
+        super(CycleGAN, self).__init__()
+        self.is_train = is_train
+        self.opt = opt
+        self.genA2B = Generator()
+        self.genB2A = Generator()
+        self.disA = Discriminator()
+        self.disB = Discriminator()
+        self.genA2B.apply(self.weights_init_normal)
+        self.genB2A.apply(self.weights_init_normal)
+        self.disA.apply(self.weights_init_normal)
+        self.disB.apply(self.weights_init_normal)
+
+        self.criterionGAN = nn.MSELoss()
+        self.criterionCycle = nn.L1Loss()
+        if self.opt.lambd_identity > 0:
+            self.criterionIdentity = nn.L1Loss()
+
+        self.optimizer_G = optim.Adam(itertools.chain(self.genA2B.parameters(), self.genB2A.parameters()), lr=self.opt.lr, betas=(0.5, 0.999))
+        self.optimizer_D = optim.Adam(itertools.chain(self.disA.parameters(), self.disB.parameters()), lr=self.opt.lr, betas=(0.5, 0.999))
+        # self.optimizers = [self.optimizer_G, self.optimizer_D]
+
+        self.lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(self.optimizer_G, lr_lambda=LambdaLR(self.opt.epochs, self.opt.start_epoch, self.opt.decay_epoch).step)
+        self.lr_scheduler_D = torch.optim.lr_scheduler.LambdaLR(self.optimizer_D, lr_lambda=LambdaLR(self.opt.epochs, self.opt.start_epoch, self.opt.decay_epoch).step)
+
+        self.fake_As = ReplayBuffer()
+        self.fake_Bs = ReplayBuffer()
+
+    def load(self, A, B):
+        self.real_A = A
+        self.real_B = B
+
+    def weights_init_normal(self,m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+
+    def forward(self):
+        self.fake_B = self.genA2B(self.real_A)
+        self.cyclic_A = self.genB2A(self.fake_B)
+        self.fake_A = self.genB2A(self.real_B)
+        self.cyclic_B = self.genA2B(self.fake_A)
+        return self.fake_B, self.cyclic_A, self.fake_A, self.cyclic_B
+
+    def backward_D(self, D, real, fake):
+        D_real = D(real)[0]
+        loss_D_real = self.criterionGAN(D_real, Tensor(1).fill_(1.0))
+        D_fake = D(fake.detach())[0]
+        loss_D_fake = self.criterionGAN(D_fake, Tensor(1).fill_(0.0))
+
+        loss_D = (loss_D_real + loss_D_fake)*0.5
+        loss_D.backward()
+        return loss_D
+
+
+    def optimze_G(self):
+        # optimize Generator: calc loss of G -> backward -> update weights
+        self.disA.set_grad(False)
+        self.disB.set_grad(False)
+
+        self.optimizer_G.zero_grad()
+        #G backward start
+        if self.opt.lambd_identity > 0:
+            identity_A = self.genB2A(self.real_A)
+            self.loss_identity_A = self.criterionIdentity(self.real_A, identity_A) * self.opt.lambd_identity
+            identity_B = self.genA2B(self.real_B)
+            self.loss_identity_B = self.criterionIdentity(self.real_B, identity_B) * self.opt.lambd_identity
+
+
+        self.loss_genA2B = self.criterionGAN(self.disB(self.fake_B)[0], Tensor(1).fill_(1.0))
+        self.loss_genB2A = self.criterionGAN(self.disA(self.fake_A)[0], Tensor(1).fill_(1.0))
+        self.loss_cyclic_A = self.criterionCycle(self.cyclic_A, self.real_A)
+        self.loss_cyclic_B = self.criterionCycle(self.cyclic_B, self.real_B)
+        self.loss_G = self.loss_genA2B + self.loss_genB2A + self.opt.lambd * (self.loss_cyclic_A + self.loss_cyclic_B)  #opt.lambd
+
+        if self.opt.lambd_identity > 0:
+            self.loss_G += self.loss_identity_A + self.loss_identity_B
+
+        self.loss_G.backward()
+
+        for group in self.optimizer_G.param_groups:
+            for p in group['params']:
+                state = self.optimizer_G.state[p]
+                if 'step' in state.keys():
+                    if(state['step']>=1022):
+                        state['step'] = 1000
+
+        self.optimizer_G.step()
+
+    def optimize_parameters(self):
+        self.forward()
+        self.optimze_G()
+        # optimize Discriminator: calc loss of D -> backward -> update weights
+        self.disA.set_grad(True)
+        self.disB.set_grad(True)
+
+        self.optimizer_D.zero_grad()
+
+        fake_A = self.fake_As.add_and_sample(self.fake_A, self.opt.batchSize) #opt.batchsize
+        #print("Loss DA")
+
+        self.loss_D_A = self.backward_D(self.disA, self.real_A, fake_A)
+        fake_B = self.fake_Bs.add_and_sample(self.fake_B, self.opt.batchSize) #opt.batchsize
+        #print("Loss DB")
+
+        self.loss_D_B = self.backward_D(self.disB, self.real_B, fake_B)
+
+        for group in self.optimizer_D.param_groups:
+            for p in group['params']:
+                state = self.optimizer_D.state[p]
+                if 'step' in state.keys():
+                    if(state['step']>=1022):
+                        state['step'] = 1000
+
+        self.optimizer_D.step()
+
+        return self.loss_D_A, self.loss_D_B, self.loss_G, self.fake_B, self.cyclic_A, self.fake_A, self.cyclic_B
---
+++ cyclegan/Train_notebook.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+import matplotlib
+matplotlib.use('Agg')
+from PIL import Image
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import numpy as np
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+import datetime
+import PIL
+import os
+import time
+import argparse
+from CycleGAN import CycleGAN
+from ImageDataset import ImageDataset
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+import argparse
+
+
+# ## Optional
+
+# In[2]:
+parser = argparse.ArgumentParser()
+#parser.add_argument('--epoch', type=int, default=10, help='starting epoch')
+parser.add_argument('--dataroot', type=str, default='../datasets/horse2zebra/',
+                    help='root directory of the dataset')
+parser.add_argument('--lr', type=float, default=0.0002,
+                    help='initial learning rate')
+parser.add_argument('--size', type=int, default=256,
+                    help='size of the data crop (squared assumed)')
+parser.add_argument('--input_nc', type=int, default=3,
+                    help='number of channels of input data')
+parser.add_argument('--output_nc', type=int, default=3,
+                    help='number of channels of output data')
+parser.add_argument('--n_cpu', type=int, default=8,
+                    help='number of cpu threads to use during batch generation')
+parser.add_argument('--batchSize', type=int, default=1, help='batch size')
+parser.add_argument('--epochs', type=int, default=200, help='number of epochs')
+parser.add_argument('--lambd', type=float, default=10,
+                    help='weighr for cycle consistency loss')
+parser.add_argument('--lambd_identity', type=float, default=5.,
+                    help='weight for identity loss, default 0 means no identity loss')
+parser.add_argument('--start_epoch', type=int, default=0,
+                    help='start_epoch for lr scheduler')
+parser.add_argument('--decay_epoch', type=int, default=100,
+                    help='decay_epoch for lr scheduler')
+
+# In[3]:
+
+
+opt = parser.parse_args()
+
+# In[4]:
+
+
+# class opt():
+#     batchSize = 4
+#     epochs = 100
+#     lr = 0.001
+#     lambd = 10
+#     cuda = True
+#     def __init__(self):
+#         pass
+
+
+# ### Hyperparameters
+
+# In[5]:
+
+
+# hp = opt()
+# hp.batchSize
+
+
+# In[6]:
+
+
+LR = 0.0002
+# batch_size = 200
+num_epochs = opt.epochs
+
+
+# ### Helper Methods
+
+# In[7]:
+
+def plot_graph(num_epochs, y_list, entity):
+    #usage : plot_graph(num_epochs,acc_list,loss_list)
+    x_list = np.arange(1, num_epochs+1)
+    file_name = entity+"_plot.png"
+    fig = plt.figure()
+    plt.plot(x_list,y_list)
+    plt.xlabel('Epoch')
+    plt.ylabel(entity)
+    plt.title('Epoch vs ' + entity)
+    plt.show()
+    plt.savefig(file_name)
+    plt.close(fig)
+
+
+# In[8]:
+
+def save_img_to_para_folder(image, epoch, batch_id, name, directory, opt):
+    folder_name = "/ld_" + str(int(opt.lambd)) + "_id_" + str(int(opt.lambd_identity))
+    directory = directory + folder_name
+    if not os.path.isdir(directory):
+        print("Create folder for parameters", directory)
+        os.mkdir(directory)
+    save_image_internal(image, epoch, batch_id, name, directory)
+
+def save_image_internal(image, epoch, batch_id, name, directory):
+    file_name = directory+"/" + \
+        str(epoch)+"_"+str(batch_id) + "_"+name + ".png"
+    image = 0.5*(image.data+1.0)
+    save_image(image, file_name)
+    # fig = plt.figure()
+    # plt.imshow(image.detach().cpu()[0].permute(1,2,0))
+    # fig.savefig(file_name, bbox_inches='tight')
+    # plt.close(fig)
+
+
+# # Main
+
+# ### Load DataSet
+
+# In[9]:
+
+
+# 256 x 256
+transform_train = [transforms.Resize(int(opt.size*1.12), Image.BICUBIC),
+                   transforms.RandomCrop(opt.size),
+                   transforms.RandomHorizontalFlip(),
+                   transforms.ToTensor(),
+                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+#transform_test = [transforms.ToTensor(),
+#                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+trainset = ImageDataset(
+    opt.dataroot, transforms_=transform_train, mode='train')
+#testset = ImageDataset(opt.dataroot, transforms_=transform_test, mode='test')
+train_loader = DataLoader(trainset, batch_size=opt.batchSize, shuffle=True)
+#test_loader = DataLoader(testset, batch_size=opt.batchSize, shuffle=True)
+print("Number of training data:", len(trainset))
+#print(len(testset))
+
+
+
+# ### Declaration
+
+# In[10]:
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = CycleGAN(opt).to(device)
+
+#process path for log, output plot and model
+dataset_name = opt.dataroot.split("/")[-2]
+directory = "train_output/" + dataset_name
+print("DATA SET NAME:", dataset_name)
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+log_name = "./logs/training_logistics_{}_ld_{}_id_{}.log".format(dataset_name, str(int(opt.lambd)), str(int(opt.lambd_identity)))
+open(log_name, 'w').close()
+
+
+# ### Training
+
+# In[11]:
+
+
+loss_A_list = []
+loss_B_list = []
+loss_G_list = []
+
+model.train()
+for epoch in range(1, num_epochs+1):
+    loss_A = 0
+    loss_B = 0
+    loss_model_G = 0
+    start_time = time.time()
+    running_loss = 0.0
+    acc = 0.0
+    print("epoch {}/{}".format(epoch, num_epochs))
+
+    for batch_idx, data in enumerate(train_loader):
+        A = data['A'].to(device)
+        B = data['B'].to(device)
+
+
+        model.load(A, B)
+        lossD_A, lossD_B, loss_G, fake_B, cyclic_A, fake_A, cyclic_B = model.optimize_parameters()
+        loss_A += lossD_A.item()
+        loss_B += lossD_B.item()
+        loss_model_G += loss_G.item()
+
+        if batch_idx % 500 == 0:
+            save_img_to_para_folder(A, epoch, batch_idx, 'input_A', directory, opt)
+            save_img_to_para_folder(B, epoch, batch_idx, 'input_B', directory, opt)
+            save_img_to_para_folder(cyclic_A, epoch, batch_idx,
+                                'cyclic_A', directory, opt)
+            save_img_to_para_folder(fake_B, epoch, batch_idx, 'fake_B', directory, opt)
+            save_img_to_para_folder(fake_A, epoch, batch_idx, 'fake_A', directory, opt)
+            save_img_to_para_folder(cyclic_B, epoch, batch_idx,
+                                'cyclic_B', directory, opt)
+    loss_A /= len(trainset)
+    loss_B /= len(trainset)
+    loss_model_G /= len(trainset)
+    loss_A_list.append(loss_A)
+    loss_B_list.append(loss_B)
+    loss_G_list.append(loss_model_G)
+    end_time = time.time()
+    result = 'TimeStamp:{},Epoch:{}, Training Time: {},lossD_A :{},lossD_B:{},loss_G:{}\n'.format(
+        time.ctime(), epoch, end_time-start_time, loss_A, loss_B, loss_model_G)
+
+    model.lr_scheduler_G.step()
+    model.lr_scheduler_D.step()
+
+    with open(log_name, "a") as myfile:
+        myfile.write(result)
+    print(result)
+    torch.save(model, "./models/C_GAN_{}_ld_{}_id_{}.model".format(dataset_name, str(int(opt.lambd)), str(int(opt.lambd_identity) )))
+
+print('-' * 20)
+plot_graph(num_epochs, loss_A_list, 'lossD_A')
+plot_graph(num_epochs, loss_B_list, 'lossD_B')
+plot_graph(num_epochs, loss_G_list, 'loss_G')
